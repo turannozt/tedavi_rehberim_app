919c23def4ce112c0ce5af1e155f5d52d7509782
turannozt <turan.ozt@outlook.com>
2024-07-24 22:46:59 +0300

    add data

---
+++ lib/data/medical_services_data.dart
@@ -0,0 +1,49 @@
+
+
+import '../constants/colors.dart';
+import '../constants/images.dart';
+import '../constants/text.dart';
+import '../model/medical_services_model.dart';
+
+List<MedicalServicesModel> medicalService = [
+  MedicalServicesModel(
+    color: AppColors.blue,
+    title: AppText.consultation,
+    image: AppImages.stethoscope,
+  ),
+  MedicalServicesModel(
+    color: AppColors.purple,
+    title: AppText.dental,
+    image: AppImages.teeth,
+  ),
+  MedicalServicesModel(
+    color: AppColors.red,
+    title: AppText.heart,
+    image: AppImages.heart,
+  ),
+  MedicalServicesModel(
+    color: AppColors.orange,
+    title: AppText.hospitals,
+    image: AppImages.clinic,
+  ),
+  MedicalServicesModel(
+    color: AppColors.green,
+    title: AppText.medicines,
+    image: AppImages.medicine,
+  ),
+  MedicalServicesModel(
+    color: AppColors.cyan,
+    title: AppText.physician,
+    image: AppImages.care,
+  ),
+  MedicalServicesModel(
+    color: AppColors.pink,
+    title: AppText.skin,
+    image: AppImages.bandage,
+  ),
+  MedicalServicesModel(
+    color: AppColors.dullOrange,
+    title: AppText.surgeon,
+    image: AppImages.syringe,
+  ),
+];
