daa065d15f553c3205d12040bd7d460f3c348d66
turannozt <turan.ozt@outlook.com>
2024-07-24 22:38:30 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/bootcampp/tedavi_rehberim/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bootcampp.tedavi_rehberim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:device_preview/device_preview.dart';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'homescreen.dart';
+
+void main() {
+  runApp(const MyApp()
+ 
+      );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
