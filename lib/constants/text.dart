dd18bf4a44d0b8eeb367bf289cbf200c4e283f00
Yavuz Soylu <100339279+yvzsylu@users.noreply.github.com>
2024-07-31 19:52:00 +0300

    translate texts

--- lib/constants/text.dart
+++ lib/constants/text.dart
@@ -1,21 +1,20 @@
 class AppText {
   AppText._();
 
-  //Coffee pics
   static const String findDoctor = 'Tedavi Rehberim';
   static const String searchDoctor = "TedAi ile Konuşmaya Başla";
   static const String open = "Aç";
   static const String closed = "Kapat";
-  static const String consultation = "Consultation";
-  static const String dental = "Dental";
+  static const String consultation = "Danışmanlık";
+  static const String dental = "Diş Hekimliği";
   static const String heart = "Kalp";
-  static const String hospitals = "Hospitals";
-  static const String medicines = "Medicines";
-  static const String physician = "Physician";
+  static const String hospitals = "Hastaneler";
+  static const String medicines = "İlaçlar";
+  static const String physician = "Hekim";
   static const String skin = "Cilt";
-  static const String surgeon = "Surgeon";
+  static const String surgeon = "Cerrah";
   static const String topDoctors = "Popüler Hastalıklar";
-  static const String viewAll = "View all";
+  static const String viewAll = "Görüntüle";
   static const String doctor1 = "Kalp Yetmezliği";
   static const String doctor2 = "Katarakt";
   static const String doctor3 = "Kulak Çınlaması";
@@ -26,10 +25,9 @@ class AppText {
   static const String hospital2 = "";
   static const String hospital3 = "";
   static const String hospital4 = "";
-  static const String doctorDescription =
-      '''dr. Gilang is one of the best doctors in the Persahabatan Hospital. He has saved more than 1000 patients in the past 3 years. He has also received many awards from domestic and abroad as the best doctors. He is available on a private or schedule. ''';
-  static const String experience = "Experience";
-  static const String patient = "Patient";
-  static const String rating = "Rating";
+  static const String doctorDescription = "";
+  static const String experience = "Tecrübe";
+  static const String patient = "Hasta";
+  static const String rating = "Puan";
   static const String makeAppointment = "Tedavi Ai 'a Sor";
 }
